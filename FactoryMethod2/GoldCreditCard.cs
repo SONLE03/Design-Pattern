91c2a85de6e352e9e6d35282a8a865cb7c9a3baf
SONLE03 <sonle102003@gmail.com>
2024-03-20 00:14:40 +0700

    update

---
+++ FactoryMethod2/GoldCreditCard.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    public class GoldCreditCard : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Gold Credit Card";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+    }
+}
