be5fb000287deda5f8df13f9e605ce303df78963
SONLE03 <sonle102003@gmail.com>
2024-03-23 15:24:03 +0700

    add prototype and composite

---
+++ Composite/IAudioTimeline.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    public interface IAudioTimeline
+    {
+        TimeSpan StartTime { get; }
+        TimeSpan EndTime { get; }
+        IEnumerable<TimeSpan> Cuts { get; }
+
+        IEnumerable<string> ToOutputLines();
+
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Prototype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCache cache = new BookCache();
+
+            // Fetch a book for the first time
+            Book book1 = cache.GetBook(123456);
+            Console.WriteLine($"Retrieved book: {book1.Title} by {book1.Author}");
+
+            // Fetch the same book again, this time it should come from the cache
+            Book book2 = cache.GetBook(123456);
+            Console.WriteLine($"Retrieved book: {book2.Title} by {book2.Author}");
+
+            // Verify that a clone was returned
+            Console.WriteLine($"Are book1 and book2 the same instance? {(book1 == book2 ? "Yes" : "No")}");
+        }
+    }
+}
