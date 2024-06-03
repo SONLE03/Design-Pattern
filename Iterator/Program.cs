6fdb33921262953335f47af7292251158170aebe
SONLE03 <sonle102003@gmail.com>
2024-06-03 23:38:59 +0700

    complete observer and iterator pattern

---
+++ Iterator/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace Iterator
+{
+    internal class Program
+    {
+        public class MenuItem
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+        }
+        //Define the Iterator and Aggregate interfaces
+        public interface IIterator<T>
+        {
+            bool HasNext();
+            T Next();
+        }
+        public interface IAggregate<T>
+        {
+            IIterator<T> CreateIterator();
+        }
+        //Implement the Menus
+        public class Menu : IAggregate<MenuItem>
+        {
+            private List<MenuItem> _items = new List<MenuItem>();
+            public void AddItem(MenuItem item)
+            {
+                _items.Add(item);
+            }
+            public IIterator<MenuItem> CreateIterator()
+            {
+                return new MenuIterator(this);
+            }
+            private class MenuIterator : IIterator<MenuItem>
+            {
+                private Menu _menu;
+                private int _currentIndex = 0;
+                public MenuIterator(Menu menu)
+                {
+                    _menu = menu;
+                }
+                public bool HasNext()
+                {
+                    return _currentIndex < _menu._items.Count;
+                }
+                public MenuItem Next()
+                {
+                    return _menu._items[_currentIndex++];
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var livingRoomMenu = new Menu();
+            livingRoomMenu.AddItem(new MenuItem { Name = "Sofa", Price = 599.99 });
+            livingRoomMenu.AddItem(new MenuItem { Name = "Coffee Table", Price = 299.99 });
+            livingRoomMenu.AddItem(new MenuItem { Name = "TV Stand", Price = 199.99 });
+
+            var bedroomMenu = new Menu();
+            bedroomMenu.AddItem(new MenuItem { Name = "Bed Frame", Price = 799.99 });
+            bedroomMenu.AddItem(new MenuItem { Name = "Dresser", Price = 499.99 });
+            bedroomMenu.AddItem(new MenuItem { Name = "Nightstand", Price = 149.99 });
+
+            var kitchenMenu = new Menu();
+            kitchenMenu.AddItem(new MenuItem { Name = "Kitchen Table", Price = 399.99 });
+            kitchenMenu.AddItem(new MenuItem { Name = "Kitchen Cabinets", Price = 1999.99 });
+            kitchenMenu.AddItem(new MenuItem { Name = "Kitchen Island", Price = 799.99 });
+
+            var menus = new List<Menu> { livingRoomMenu, bedroomMenu, kitchenMenu };
+            foreach (var menu in menus)
+            {
+                var iterator = menu.CreateIterator();
+                while (iterator.HasNext())
+                {
+                    var item = iterator.Next();
+                    Console.WriteLine($"Item: {item.Name}, Price: ${item.Price}");
+                }
+            }
+        }
+    }
+}
---
+++ Observer/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Observer
+{
+    internal class Program
+    {
+        public interface ICustomer
+        {
+            void Notify(Product product);
+        }
+        // Concrete Observer: Customer
+        public class Customer : ICustomer
+        {
+            public string CustomerName { get; private set; }
+            public Customer(string name)
+            {
+                CustomerName = name;
+            }
+            public void Notify(Product product)
+            {
+                Console.WriteLine($"{CustomerName}, the price of {product.ProductName} has changed to ${product.Price}!");
+            }
+        }
+        // Subject Interface
+        public interface IProduct
+        {
+            void AddWatcher(ICustomer customer);
+            void RemoveWatcher(ICustomer customer);
+            void NotifyWatchers();
+        }
+        // Concrete Subject: Product
+        public class Product : IProduct
+        {
+            private List<ICustomer> _customers = new List<ICustomer>();
+            public string ProductName { get; private set; }
+            private double _price;
+            public Product(string name, double price)
+            {
+                ProductName = name;
+                _price = price;
+            }
+            public double Price
+            {
+                get { return _price; }
+                set
+                {
+                    if (_price != value)
+                    {
+                        _price = value;
+                        NotifyWatchers();
+                    }
+                }
+            }
+            public void AddWatcher(ICustomer customer)
+            {
+                _customers.Add(customer);
+            }
+            public void RemoveWatcher(ICustomer customer)
+            {
+                _customers.Remove(customer);
+            }
+            public void NotifyWatchers()
+            {
+                foreach (var customer in _customers)
+                {
+                    customer.Notify(this);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Product TShirt = new Product("T-Shirt", 30.00);
+            Customer Alice = new Customer("Alice");
+            Customer Bob = new Customer("Bob");
+            Customer Charlie = new Customer("Charlie");
+
+            // AddWatcher
+            TShirt.AddWatcher(Alice);   // Alice wants to watch the price of the T-Shirt
+            TShirt.AddWatcher(Bob);     // Bob wants to watch the price of the T-Shirt
+            TShirt.AddWatcher(Charlie); // Charlie also wants to watch the price of the T-Shirt
+
+            // RemoveWatcher
+            TShirt.RemoveWatcher(Bob);
+
+            // Simulating a price change
+            TShirt.Price = 25.00;
+
+            Console.ReadKey();
+        }
+    }
+}
